9c50d35ffd4891d4483e5434412c5fea091c111c
Vu Duy Lam <vuduylam250302@gmail.com>
2025-12-28 11:28:40 +0700

    feat: add GPS reading functionality

--- SPK-ESP32/SPK-ESP32.ino
+++ SPK-ESP32/SPK-ESP32.ino
@@ -1,9 +1,47 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <TinyGPSPlus.h>
 
+//================GPS================
+#define GPS_RX_PIN
+#define GPS_TX_PIN
+#define GPS_BAUD 9600
+
+//==============Serial===============
+#define SERIAL_MONITOR Serial
+#define SERIAL_GPS Serial1
+
+//================ ================
+void getGps(float& latitude, float& longitude);
+
+//================ ================
+TinyGPSPlus gps;
+
+void setup() {
+  SERIAL_MONITOR.begin(115200);
+  SERIAL_GPS.begin(GPS_BAUD, SERIAL_8N1, GPS_RX_PIN, GPS_TX_PIN);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+}
+
+void getGps(float& latitude, float& longitude){
+  boolean newData = false;
+  for (unsigned long start = millis(); millis() - start < 1000;){
+    while (neogps.available()){
+      if (gps.encode(neogps.read()))
+        if (gps.location.isValid()){
+          newData = true;
+          break;
+        }
+    }
+  }
+  
+  if (newData) {
+    latitude = gps.location.lat();
+    longitude = gps.location.lng();
+    newData = false;
+  }
+  else {
+    Serial.println("No GPS data is available");
+  }
 }
