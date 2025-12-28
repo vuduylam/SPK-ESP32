ec13e47ec83064b88c380806c4f6dac46097cdbe
Vu Duy Lam <vuduylam250302@gmail.com>
2025-12-28 22:40:45 +0700

    feat: add firebase connection feature through sim7600
    - Initialize GSM modem function.
    - Set data to Firebase real-time database.
    - Get data from Firebase real-time database.
    - Remove Firebase connection through  WiFi.

--- SPK-ESP32/SPK-ESP32.ino
+++ SPK-ESP32/SPK-ESP32.ino
@@ -1,19 +1,41 @@
+#define ENABLE_USER_AUTH
+#define ENABLE_DATABASE
+#define ENABLE_GSM_NETWORK
+#define ENABLE_ESP_SSLCLIENT
+
+#define TINY_GSM_MODEM_SIM7600
+
+#include <Arduino.h>
+#include <FirebaseClient.h>
 #include <TinyGPSPlus.h>
-#include <SSLClient.h>
-#include <WiFiClientSecure.h>
+#include <TinyGsmClient.h>
 
 //================GPS================
 #define GPS_RX_PIN 34
 #define GPS_TX_PIN 35
 #define GPS_BAUD 9600
 
+//==============Modem===============
+#define SIM_MODEM_BAUD 115200
+#define SIM_MODEM_RST 5
+#define SIM_MODEM_RST_LOW true // active LOW
+#define SIM_MODEM_RST_DELAY 200
+#define SIM_MODEM_TX 26
+#define SIM_MODEM_RX 27
 
 //==============Serial===============
 #define SERIAL_MONITOR Serial
 #define SERIAL_GPS Serial1
+#define SERIAL_AT Serial2
 
-#define WIFI_SSID "Phuongthao_2.4G"
-#define WIFI_PASSWORD "xitrum25032002"
+#define TINY_GSM_DEBUG SERIAL_MONITOR
+#define TINY_GSM_USE_GPRS true
+#define TINY_GSM_USE_WIFI false
+
+//=========GPRS credentials, if any========
+const char apn[] = "m3-world";
+const char gprsUser[] = "";
+const char gprsPass[] = "";
 
 //==============Firebase===============
 #define API_KEY "AIzaSyBuLticw5nMQeC2KftZkPhg2k5bJfa8K48"
@@ -21,34 +43,68 @@
 #define USER_PASSWORD "123456"
 #define DATABASE_URL "https://test-a2978-default-rtdb.asia-southeast1.firebasedatabase.app/"
 
-//================ ================
-void getFirebaseData()
+//================Function declaration================
+void getFirebaseData();
 void getGps(float& latitude, float& longitude);
 float getDistance(float latitude1, float longtitude1, float latitude2, float longtitude2);
 void processData(AsyncResult &aResult);
+void initModem();
+void auth_debug_print(AsyncResult &aResult) {
+    if (aResult.isEvent())
+    {
+        Firebase.printf("Event task: %s, msg: %s, code: %d\n", aResult.uid().c_str(), aResult.eventLog().message().c_str(), aResult.eventLog().code());
+    }
+
+    if (aResult.isDebug())
+    {
+        Firebase.printf("Debug task: %s, msg: %s\n", aResult.uid().c_str(), aResult.debug().c_str());
+    }
+
+    if (aResult.isError())
+    {
+        Firebase.printf("Error task: %s, msg: %s, code: %d\n", aResult.uid().c_str(), aResult.error().message().c_str(), aResult.error().code());
+    }
+}
 
 //================ ================
 TinyGPSPlus gps;
 
-SSL_CLIENT ssl_client;
+TinyGsm modem(SERIAL_AT);
+TinyGsmClient gsm_client(modem, 0), stream_gsm_client(modem, 1);
+ESP_SSLClient ssl_client, stream_ssl_client;
 
 using AsyncClient = AsyncClientClass;
-AsyncClient aClient(ssl_client);
+AsyncClient aClient(ssl_client), streamClient(stream_ssl_client);
 
 UserAuth user_auth(API_KEY, USER_EMAIL, USER_PASSWORD, 3000);
 FirebaseApp app;
 RealtimeDatabase Database;
-AsyncResult databaseResult;
+AsyncResult streamResult;
 
-//================ ================
+//================Global Variable================
+String phoneNumber = "";
+unsigned long ms = 0;
 
+//===============================================
 void setup() {
+  //UART settings
   SERIAL_MONITOR.begin(115200);
   SERIAL_GPS.begin(GPS_BAUD, SERIAL_8N1, GPS_RX_PIN, GPS_TX_PIN);
+  SERIAL_AT.begin(SIM_MODEM_BAUD, SERIAL_8N1, SIM_MODEM_RX, SIM_MODEM_TX);
+
+  initModem();
 
   Firebase.printf("Firebase Client v%s\n", FIREBASE_CLIENT_VERSION);
 
-  set_ssl_client_insecure_and_buffer(ssl_client);
+  ssl_client.setInsecure();
+  ssl_client.setDebugLevel(1);
+  ssl_client.setBufferSizes(2048 /* rx */, 1024 /* tx */);
+  ssl_client.setClient(&gsm_client);
+
+  stream_ssl_client.setInsecure();
+  stream_ssl_client.setDebugLevel(1);
+  stream_ssl_client.setBufferSizes(2048 /* rx */, 1024 /* tx */);
+  stream_ssl_client.setClient(&stream_gsm_client);
 
   Serial.println("Initializing app...");
   initializeApp(aClient, app, getAuth(user_auth), auth_debug_print, "🔐 authTask");
@@ -56,10 +112,31 @@ void setup() {
   app.getApp<RealtimeDatabase>(Database);
 
   Database.url(DATABASE_URL);
+
+  streamClient.setSSEFilters("get,put,patch,keep-alive,cancel,auth_revoked");
+
+  Database.get(streamClient, "/examples/Stream/data", processData, true, "streamTask");
 }
 
 void loop() {
+  // To maintain the authentication and async tasks
+  app.loop();
 
+  if (millis() - ms > 20000 && app.ready()) {
+    ms = millis();
+
+    JsonWriter writer;
+
+    object_t json, obj1, obj2;
+
+    writer.create(obj1, "ms", ms);
+    writer.create(obj2, "rand", random(10000, 30000));
+    writer.join(json, 2, obj1, obj2);
+
+    Database.set<object_t>(aClient, "/examples/Stream/data", json, processData, "setTask");
+    Serial.println("========= Data ==========");
+    getFirebaseData();
+  }
 }
 
 template <typename T>
@@ -75,8 +152,8 @@ void check_and_print_value(T value) {
 }
 
 void getFirebaseData() {
-    stringValue = Database.get<String>(aClient, "/examples/test");
-    check_and_print_value(stringValue);
+    phoneNumber = Database.get<String>(aClient, "/examples/test");
+    check_and_print_value(phoneNumber);
 }
 
 void getGps(float& latitude, float& longitude) {
@@ -126,4 +203,103 @@ float getDistance(float latitude1, float longtitude1, float latitude2, float lon
   distCalc*=6371000.0; //Converting to meters
 
   return distCalc;
+}
+
+void initModem() {
+  // Resetting the modem
+  #if defined(SIM_MODEM_RST)
+      pinMode(SIM_MODEM_RST, SIM_MODEM_RST_LOW ? OUTPUT_OPEN_DRAIN : OUTPUT);
+      digitalWrite(SIM_MODEM_RST, SIM_MODEM_RST_LOW);
+      delay(100);
+      digitalWrite(SIM_MODEM_RST, !SIM_MODEM_RST_LOW);
+      delay(3000);
+      digitalWrite(SIM_MODEM_RST, SIM_MODEM_RST_LOW);
+  #endif
+
+  DBG("Wait...");
+  delay(3000);
+
+  DBG("Initializing modem...");
+  while (!modem.init()) {
+      SERIAL_MONITOR.println("Modem init failed! Retrying...");
+      delay(2000);
+  }
+
+  SERIAL_MONITOR.println("Modem initialized successfully!");
+  /**
+    * 2 Automatic
+    * 13 GSM Only
+    * 14 WCDMA Only
+    * 38 LTE Only
+    */
+  modem.setNetworkMode(2);
+  if (modem.waitResponse(10000L) != 1) {
+      DBG(" setNetworkMode faill");
+  }
+
+  String name = modem.getModemName();
+  DBG("Modem Name:", name);
+
+  String modemInfo = modem.getModemInfo();
+  DBG("Modem Info:", modemInfo);
+
+  SERIAL_MONITOR.print("Waiting for network...");
+  while (!modem.waitForNetwork()) {
+      SERIAL_MONITOR.println("Get network failed! Retrying...");
+      delay(2000);
+  }
+  SERIAL_MONITOR.println("Get network successfully!");
+
+  //Connect GPRS
+  SERIAL_MONITOR.print("Connecting to APN: ");
+  SERIAL_MONITOR.println(apn);
+  while (!modem.gprsConnect(apn, gprsUser, gprsPass)){
+      SERIAL_MONITOR.println("GPRS connect failed");
+      delay(2000);
+  }
+  SERIAL_MONITOR.println("GPRS connect successfully");
+
+  if (modem.isNetworkConnected())
+      SERIAL_MONITOR.println("Network connected");
+}
+void processData(AsyncResult &aResult) {
+  // Exits when no result is available when calling from the loop.
+  if (!aResult.isResult())
+    return;
+
+  if (aResult.isEvent()) {
+    Firebase.printf("Event task: %s, msg: %s, code: %d\n", aResult.uid().c_str(), aResult.eventLog().message().c_str(), aResult.eventLog().code());
+  }
+
+  if (aResult.isDebug()) {
+    Firebase.printf("Debug task: %s, msg: %s\n", aResult.uid().c_str(), aResult.debug().c_str());
+  }
+
+  if (aResult.isError()) {
+      Firebase.printf("Error task: %s, msg: %s, code: %d\n", aResult.uid().c_str(), aResult.error().message().c_str(), aResult.error().code());
+  }
+
+  if (aResult.available()) {
+    RealtimeDatabaseResult &stream = aResult.to<RealtimeDatabaseResult>();
+    if (stream.isStream()) {
+      Serial.println("----------------------------");
+      Firebase.printf("task: %s\n", aResult.uid().c_str());
+      Firebase.printf("event: %s\n", stream.event().c_str());
+      Firebase.printf("path: %s\n", stream.dataPath().c_str());
+      Firebase.printf("data: %s\n", stream.to<const char *>());
+      Firebase.printf("type: %d\n", stream.type());
+
+      // The stream event from RealtimeDatabaseResult can be converted to the values as following.
+      bool v1 = stream.to<bool>();
+      int v2 = stream.to<int>();
+      float v3 = stream.to<float>();
+      double v4 = stream.to<double>();
+      String v5 = stream.to<String>();
+    }
+    else {
+      Serial.println("----------------------------");
+      Firebase.printf("task: %s, payload: %s\n", aResult.uid().c_str(), aResult.c_str());
+    }
+    Firebase.printf("Free Heap: %d\n", ESP.getFreeHeap());
+  }
 }
