9db4c53e70e666a026e31e2c23778d6b8c2fd212
Vu Duy Lam <vuduylam250302@gmail.com>
2025-12-28 15:33:14 +0700

    feat: add Firebase real-time database
    - Connect to Firebase real-time database

--- SPK-ESP32/SPK-ESP32.ino
+++ SPK-ESP32/SPK-ESP32.ino
@@ -1,24 +1,60 @@
 #include <TinyGPSPlus.h>
+#include <SSLClient.h>
+#include <WiFiClientSecure.h>
 
 //================GPS================
-#define GPS_RX_PIN 14
-#define GPS_TX_PIN 15
+#define GPS_RX_PIN 34
+#define GPS_TX_PIN 35
 #define GPS_BAUD 9600
+
 
 //==============Serial===============
 #define SERIAL_MONITOR Serial
 #define SERIAL_GPS Serial1
 
+#define WIFI_SSID "Phuongthao_2.4G"
+#define WIFI_PASSWORD "xitrum25032002"
+
+//==============Firebase===============
+#define API_KEY "AIzaSyBuLticw5nMQeC2KftZkPhg2k5bJfa8K48"
+#define USER_EMAIL "vuduylam250302@gmail.com"
+#define USER_PASSWORD "123456"
+#define DATABASE_URL "https://test-a2978-default-rtdb.asia-southeast1.firebasedatabase.app/"
+
 //================ ================
 void getGps(float& latitude, float& longitude);
 float getDistance(float latitude1, float longtitude1, float latitude2, float longtitude2);
+void processData(AsyncResult &aResult);
 
 //================ ================
 TinyGPSPlus gps;
 
+SSL_CLIENT ssl_client;
+
+using AsyncClient = AsyncClientClass;
+AsyncClient aClient(ssl_client);
+
+UserAuth user_auth(API_KEY, USER_EMAIL, USER_PASSWORD, 3000);
+FirebaseApp app;
+RealtimeDatabase Database;
+AsyncResult databaseResult;
+
+//================ ================
+
 void setup() {
   SERIAL_MONITOR.begin(115200);
   SERIAL_GPS.begin(GPS_BAUD, SERIAL_8N1, GPS_RX_PIN, GPS_TX_PIN);
+
+  Firebase.printf("Firebase Client v%s\n", FIREBASE_CLIENT_VERSION);
+
+  set_ssl_client_insecure_and_buffer(ssl_client);
+
+  Serial.println("Initializing app...");
+  initializeApp(aClient, app, getAuth(user_auth), auth_debug_print, "🔐 authTask");
+
+  app.getApp<RealtimeDatabase>(Database);
+
+  Database.url(DATABASE_URL);
 }
 
 void loop() {
